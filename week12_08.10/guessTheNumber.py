50e62c666c0e1c5ad4f2a61b2c7967628f20594b
anushka-pande <anushkapande04@gmail.com>
2024-10-11 16:50:49 +0530

    pr12 code1

--- week12_08.10/guessTheNumber.py
+++ week12_08.10/guessTheNumber.py
@@ -5,17 +5,17 @@ def guessTheNumber():
 	print("I have chosen a number between 1 to 10. Try to guess it!")
 	magic_num = random.randint(0, 10)
 	
-	num_input = input("Enter your choice of number between 1 to 10: ")
+	num_input = input("Enter your choice between 0 to 10: ")
 	if num_input.isdigit():
 		num = int(num_input)
 		if num < 1 or num > 10:
-			return "Invalid Choice! Please choose a number between 1 to 10."
+			return "Invalid Choice! Please choose a number between the range of 0 to 10."
 		else:
 			if num == magic_num:
 				return "Congratulations! You guessed the number correctly."
 			else:
 				return f"Oops! The correct number was {magic_num}. Better luck next time!"
 	else:
-		return "Please enter a valid integer between 1 to 10."
+		return "Please enter a valid integer between the range of  0 to 10."
 
 print(guessTheNumber())
